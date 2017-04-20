971df238d14653111589239ef3bd7f01d4508bec
Jeroen Akkerman <jeroen.akkerman@easyflex.nl>
2017-04-20 11:37:41 +0200

    Use backtics around the url in /i

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -5,7 +5,7 @@ import { logCommand } from '../helpers/command-logger';
 export async function infoFunction(discordMessage: Discord.Message) {
   await discordMessage.channel.sendMessage('Greetings, I am MarketBot!\n' +
     `I was created by <@${creator.id}> to fetch data from the EVE Online market, ` +
-    'all my data currently comes from https://eve-central.com, the EVE Swagger Interface ' +
+    'all my data currently comes from EVE-Central, the EVE Swagger Interface ' +
     'and the Static Data Export provided by CCP.\n\n' +
     'You can access my functions by using these commands:\n\n' +
     `- \`${priceCommand} <item name> [${regionCommand} <region name>]\` ` +
@@ -15,6 +15,6 @@ export async function infoFunction(discordMessage: Discord.Message) {
     '- When issued with this command, I will search a regional market for the best sell orders available.' +
     '\n**Warning! This does not include Citadels**\n\n' +
     `- \`${infoCommand}\` - Print this information.\n\n` +
-    'My code is publicly available on https://github.com/Ionaru/MarketBot');
+    'My code is publicly available on `https://github.com/Ionaru/MarketBot`');
   logCommand('info', discordMessage);
 }
