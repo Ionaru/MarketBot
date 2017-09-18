70d25a8c0a6228d992143cb5a1c3dafca85bfc89
Ionaru <Jeroen_akkerman1@hotmail.com>
2017-09-18 22:17:47 +0200

    Fix guess function tests & no longer compile test sources on build
    Fixes #1

--- src/helpers/guessers.spec.ts
+++ src/helpers/guessers.spec.ts
@@ -1,8 +1,8 @@
 import { assert } from 'chai';
+import * as Fuse from 'fuse.js';
 import { assert as sAssert, SinonSpy, SinonStub, spy, stub } from 'sinon';
 import { logger, WinstonPnPLogger } from 'winston-pnp-logger';
 
-import * as Fuse from 'fuse.js';
 import { typeIDsPath } from '../market-bot';
 import { guessUserItemInput } from './guessers';
 import { loadItems } from './items-loader';
@@ -38,28 +38,32 @@ describe('Guess user item input', function(this: any) {
     it('It should correctly return a correctly spelled item', () => {
       const result = guessUserItemInput(itemName);
       assert.isObject(result);
-      assert.equal(result.name.en, itemName);
+      assert.equal(result.itemData.name.en, itemName);
+      assert.isFalse(result.guess);
       sAssert.notCalled(fuseSearchFunction);
     });
 
     it('It should correctly return an item with just the start typed', () => {
       const result = guessUserItemInput(itemName.substring(0, 4));
       assert.isObject(result);
-      assert.equal(result.name.en, itemName);
+      assert.equal(result.itemData.name.en, itemName);
+      assert.isFalse(result.guess);
       sAssert.notCalled(fuseSearchFunction);
     });
 
     it('It should correctly return an item with just the end typed', () => {
       const result = guessUserItemInput(itemName.substring(4, itemName.length));
       assert.isObject(result);
-      assert.equal(result.name.en, itemName);
+      assert.equal(result.itemData.name.en, itemName);
+      assert.isFalse(result.guess);
       sAssert.notCalled(fuseSearchFunction);
     });
 
     it('It should correctly return an item with just the middle typed', () => {
       const result = guessUserItemInput(itemName.substring(1, itemName.length - 1));
       assert.isObject(result);
-      assert.equal(result.name.en, itemName);
+      assert.equal(result.itemData.name.en, itemName);
+      assert.isFalse(result.guess);
       sAssert.notCalled(fuseSearchFunction);
     });
   });
@@ -81,13 +85,16 @@ describe('Guess user item input', function(this: any) {
     it('It should correctly return a misspelled item', () => {
       const result = guessUserItemInput('Backinbaw');
       assert.isObject(result);
-      assert.equal(result.name.en, itemName);
+      assert.equal(result.itemData.name.en, itemName);
+      assert.isTrue(result.guess);
       sAssert.calledOnce(fuseSearchFunction);
     });
 
     it('It should return nothing when given a horribly misspelled item', () => {
       const result = guessUserItemInput('/////////');
-      assert.isUndefined(result);
+      assert.isObject(result);
+      assert.isUndefined(result.itemData);
+      assert.isTrue(result.guess);
       sAssert.calledOnce(fuseSearchFunction);
     });
   });
@@ -112,14 +119,16 @@ describe('Guess user item input', function(this: any) {
     it('It should resolve a shortcut to an item', () => {
       const result = guessUserItemInput(shortcut);
       assert.isObject(result);
-      assert.equal(result.name.en, itemName);
+      assert.equal(result.itemData.name.en, itemName);
+      assert.isFalse(result.guess);
       sAssert.notCalled(fuseSearchFunction);
     });
 
     it('It should be able to resolve t2 versions of shortcuts', () => {
       const result = guessUserItemInput(shortcutT2);
       assert.isObject(result);
-      assert.equal(result.name.en, itemNameT2);
+      assert.equal(result.itemData.name.en, itemNameT2);
+      assert.isFalse(result.guess);
       sAssert.notCalled(fuseSearchFunction);
     });
   });
