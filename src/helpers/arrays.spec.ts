1a9a758ad92e223c32f55dc3f2e22300069f7e67
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2019-03-11 13:56:37 +0100

    Convert arrays.spec to Jest tests and add more tests.

--- src/helpers/arrays.spec.ts
+++ src/helpers/arrays.spec.ts
@@ -1,5 +1,3 @@
-import { assert } from 'chai';
-
 import { sortArrayByObjectProperty, sortArrayByObjectPropertyLength } from './arrays';
 
 describe('Array sorting', () => {
@@ -13,10 +11,22 @@ describe('Array sorting', () => {
       {value: 1},
     ];
 
-    it('should sort the array by the object property', () => {
+    test('Sorting an array with only one value', () => {
+      const sortedArray = sortArrayByObjectProperty([{value: 3}], 'value');
+
+      expect(sortedArray).toStrictEqual([{value: 3}]);
+    });
+
+    test('Reverse sorting an array with only one value', () => {
+      const sortedArray = sortArrayByObjectProperty([{value: 3}], 'value', true);
+
+      expect(sortedArray).toStrictEqual([{value: 3}]);
+    });
+
+    test('Sorting an array by object property', () => {
       const sortedArray = sortArrayByObjectProperty(unsortedArray, 'value');
 
-      assert.deepEqual(sortedArray, [
+      expect(sortedArray).toStrictEqual([
         {value: 0},
         {value: 1},
         {value: 2},
@@ -25,10 +35,10 @@ describe('Array sorting', () => {
       ]);
     });
 
-    it('should reverse sort the array by the object property', () => {
+    test('Reverse sorting an array by object property', () => {
       const sortedArray = sortArrayByObjectProperty(unsortedArray, 'value', true);
 
-      assert.deepEqual(sortedArray, [
+      expect(sortedArray).toStrictEqual([
         {value: 4},
         {value: 3},
         {value: 2},
@@ -37,7 +47,7 @@ describe('Array sorting', () => {
       ]);
     });
 
-    it('should sort the array if two properties are equal', () => {
+    test('Sort an array with some equal values', () => {
       const unsortedArrayWithEqualValue = [
         {value: 2},
         {value: 3},
@@ -48,7 +58,7 @@ describe('Array sorting', () => {
 
       const sortedArray = sortArrayByObjectProperty(unsortedArrayWithEqualValue, 'value');
 
-      assert.deepEqual(sortedArray, [
+      expect(sortedArray).toStrictEqual([
         {value: 1},
         {value: 2},
         {value: 2},
@@ -57,7 +67,47 @@ describe('Array sorting', () => {
       ]);
     });
 
-    it('should reverse sort the array if two properties are equal', () => {
+    test('Sorting an array with only equal values', () => {
+      const unsortedArrayWithEqualValues = [
+        {value: 2},
+        {value: 2},
+        {value: 2},
+        {value: 2},
+        {value: 2},
+      ];
+
+      const sortedArray = sortArrayByObjectProperty(unsortedArrayWithEqualValues, 'value');
+
+      expect(sortedArray).toStrictEqual([
+        {value: 2},
+        {value: 2},
+        {value: 2},
+        {value: 2},
+        {value: 2},
+      ]);
+    });
+
+    test('Reverse sorting an array with only equal values', () => {
+      const unsortedArrayWithEqualValues = [
+        {value: 2},
+        {value: 2},
+        {value: 2},
+        {value: 2},
+        {value: 2},
+      ];
+
+      const sortedArray = sortArrayByObjectProperty(unsortedArrayWithEqualValues, 'value', true);
+
+      expect(sortedArray).toStrictEqual([
+        {value: 2},
+        {value: 2},
+        {value: 2},
+        {value: 2},
+        {value: 2},
+      ]);
+    });
+
+    test('Reverse sort an array with some equal values', () => {
       const unsortedArrayWithEqualValue = [
         {value: 2},
         {value: 3},
@@ -68,12 +118,100 @@ describe('Array sorting', () => {
 
       const sortedArray = sortArrayByObjectProperty(unsortedArrayWithEqualValue, 'value', true);
 
-      assert.deepEqual(sortedArray, [
+      expect(sortedArray).toStrictEqual([
         {value: 4},
         {value: 3},
         {value: 2},
         {value: 2},
         {value: 1},
+      ]);
+    });
+
+    test('Attempting to sort an undefined value in an array must throw an error', () => {
+      const unsortedArrayWithUndefinedValue = [
+        {value: 2},
+        {value: 3},
+        {value: undefined},
+        {value: 4},
+        {value: 1},
+      ];
+
+      expect(() => {
+        sortArrayByObjectProperty(unsortedArrayWithUndefinedValue, 'value');
+      }).toThrow(new Error('Unable to compare values \'3\' and \'undefined\''));
+    });
+
+    test('Attempting to sort a string value in an array must throw an error', () => {
+      const unsortedArrayWithStringValue = [
+        {value: 2},
+        {value: 3},
+        {value: '2'},
+        {value: 4},
+        {value: 1},
+      ];
+
+      expect(() => {
+        sortArrayByObjectProperty(unsortedArrayWithStringValue, 'value');
+      }).toThrow(new Error('Unable to compare different types: \'3\' (number) and \'2\' (string)'));
+    });
+
+    test('Sort an array with string values', () => {
+      const unsortedArrayWithStringValues = [
+        {value: '2'},
+        {value: '3'},
+        {value: '2'},
+        {value: '4'},
+        {value: '1'},
+      ];
+
+      const sortedArray = sortArrayByObjectProperty(unsortedArrayWithStringValues, 'value');
+
+      expect(sortedArray).toStrictEqual([
+        {value: '1'},
+        {value: '2'},
+        {value: '2'},
+        {value: '3'},
+        {value: '4'},
+      ]);
+    });
+
+    test('Sort an array with nested values', () => {
+      const unsortedArrayWithEqualValue = [
+        {value: {value: 2}},
+        {value: {value: 3}},
+        {value: {value: 2}},
+        {value: {value: 4}},
+        {value: {value: 1}},
+      ];
+
+      const sortedArray = sortArrayByObjectProperty(unsortedArrayWithEqualValue, 'value.value', true);
+
+      expect(sortedArray).toStrictEqual([
+        {value: {value: 4}},
+        {value: {value: 3}},
+        {value: {value: 2}},
+        {value: {value: 2}},
+        {value: {value: 1}},
+      ]);
+    });
+
+    test('Sort an array with double nested values', () => {
+      const unsortedArrayWithEqualValue = [
+        {value: {value: {value: 2}}},
+        {value: {value: {value: 3}}},
+        {value: {value: {value: 2}}},
+        {value: {value: {value: 4}}},
+        {value: {value: {value: 1}}},
+      ];
+
+      const sortedArray = sortArrayByObjectProperty(unsortedArrayWithEqualValue, 'value.value.value', true);
+
+      expect(sortedArray).toStrictEqual([
+        {value: {value: {value: 4}}},
+        {value: {value: {value: 3}}},
+        {value: {value: {value: 2}}},
+        {value: {value: {value: 2}}},
+        {value: {value: {value: 1}}},
       ]);
     });
   });
@@ -87,10 +225,10 @@ describe('Array sorting', () => {
       {value: '1234'},
     ];
 
-    it('should sort the array by the object property length', () => {
+    test('should sort the array by the object property length', () => {
       const sortedArray = sortArrayByObjectPropertyLength(unsortedArray, 'value');
 
-      assert.deepEqual(sortedArray, [
+      expect(sortedArray).toStrictEqual([
         {value: '1'},
         {value: '12'},
         {value: '123'},
@@ -99,10 +237,10 @@ describe('Array sorting', () => {
       ]);
     });
 
-    it('should reverse sort the array by the object property length', () => {
+    test('should reverse sort the array by the object property length', () => {
       const sortedArray = sortArrayByObjectPropertyLength(unsortedArray, 'value', true);
 
-      assert.deepEqual(sortedArray, [
+      expect(sortedArray).toStrictEqual([
         {value: '12345'},
         {value: '1234'},
         {value: '123'},
@@ -111,7 +249,7 @@ describe('Array sorting', () => {
       ]);
     });
 
-    it('should sort the array by the object property length with equal length values', () => {
+    test('should sort the array by the object property length with equal length values', () => {
       const unsortedArrayWithEqualValues = [
         {value: '12345'},
         {value: '12'},
@@ -123,7 +261,7 @@ describe('Array sorting', () => {
 
       const sortedArray = sortArrayByObjectPropertyLength(unsortedArrayWithEqualValues, 'value');
 
-      assert.deepEqual(sortedArray, [
+      expect(sortedArray).toStrictEqual([
         {value: '1'},
         {value: '12'},
         {value: '12'},
@@ -133,7 +271,7 @@ describe('Array sorting', () => {
       ]);
     });
 
-    it('should reverse sort the array by the object property length with equal length values', () => {
+    test('should reverse sort the array by the object property length with equal length values', () => {
       const unsortedArrayWithEqualValues = [
         {value: '12345'},
         {value: '12'},
@@ -145,7 +283,7 @@ describe('Array sorting', () => {
 
       const sortedArray = sortArrayByObjectPropertyLength(unsortedArrayWithEqualValues, 'value', true);
 
-      assert.deepEqual(sortedArray, [
+      expect(sortedArray).toStrictEqual([
         {value: '12345'},
         {value: '1234'},
         {value: '123'},
