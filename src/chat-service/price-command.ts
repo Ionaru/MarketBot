5a61d98e0c7bd7efff9957c4ba38c630fcd5b986
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2019-04-19 11:18:13 +0200

    WIP add command logic (disabled)

--- src/chat-service/price-command.ts
+++ src/chat-service/price-command.ts
@@ -32,5 +32,101 @@ export class PriceCommand extends Command {
             embed.addField('Error', 'You need to give me an item to search for.');
             return;
         }
+
+        // const reply = new Discord.RichEmbed();
+        //
+        // let locationName = '';
+        //
+        // if (!(messageData.item && messageData.item.length)) {
+        //     reply.addField('Error', 'You need to give me an item to search for.');
+        //     return {reply, itemData: undefined, locationName};
+        // }
+        //
+        // const {itemData, guess, id}: IGuessReturn = await guessItemInput(messageData.item);
+        //
+        // const guessHint = getGuessHint({itemData, guess, id}, messageData.item);
+        // if (guessHint) {
+        //     reply.addField('Warning', guessHint);
+        // }
+        //
+        // if (!itemData.id) {
+        //     return {reply, itemData: undefined, locationName};
+        // }
+        //
+        // const defaultLocation = regions.filter((region) => region.name === 'The Forge')[0];
+        // let location = defaultLocation;
+        //
+        // if (messageData.region) {
+        //     location = (await guessRegionInput(messageData.region)).itemData;
+        //     if (!location.id) {
+        //         location = defaultLocation;
+        //         reply.addField('Warning', `I don't know of the "${messageData.region}" region, defaulting to ${regionFormat(location.name)}`);
+        //     }
+        // }
+        //
+        // if (messageData.system) {
+        //     location = (await guessSystemInput(messageData.system)).itemData;
+        //     if (!location.id) {
+        //         location = defaultLocation;
+        //         reply.addField('Warning', `I don't know of the "${messageData.system}" system, defaulting to ${regionFormat(location.name)}`);
+        //     }
+        // }
+        //
+        // locationName = location.name;
+        //
+        // const json = await fetchPriceData(itemData.id, location.id);
+        //
+        // if (!(json && json.length)) {
+        //     reply.addField('Error', `My apologies, I was unable to fetch the required data from the web, please try again later.`);
+        //     return {reply, itemData, locationName};
+        // }
+        //
+        // const sellData: IPriceData = json[0].sell;
+        // const buyData: IPriceData = json[0].buy;
+        //
+        // let sellPrice = 'unknown';
+        // let lowestSellPrice = 'unknown';
+        // if (sellData.fivePercent && sellData.fivePercent !== 0) {
+        //     sellPrice = formatNumber(sellData.wavg) + ' ISK';
+        //     lowestSellPrice = formatNumber(sellData.fivePercent) + ' ISK';
+        // }
+        //
+        // let buyPrice = 'unknown';
+        // let highestBuyPrice = 'unknown';
+        // if (buyData.fivePercent && buyData.fivePercent !== 0) {
+        //     buyPrice = formatNumber(buyData.wavg) + ' ISK';
+        //     highestBuyPrice = formatNumber(buyData.fivePercent) + ' ISK';
+        // }
+        //
+        // if (sellPrice === 'unknown' && buyPrice === 'unknown') {
+        //     const itemName = itemFormat(itemData.name);
+        //     const replyText = `I couldn't find any price information for ${itemName} in ${regionFormat(locationName)}, sorry.`;
+        //     reply.addField('No data', replyText);
+        //     return {reply, itemData, locationName};
+        // }
+        //
+        // reply.setAuthor(itemData.name, `http://data.saturnserver.org/eve/Icons/UI/WindowIcons/wallet.png`);
+        // reply.setDescription(`Price information for ${regionFormat(locationName)}`);
+        // reply.setThumbnail(`https://image.eveonline.com/Type/${itemData.id}_64.png`);
+        //
+        // let sellInfo = '';
+        // if (sellPrice !== 'unknown') {
+        //     sellInfo += `* Lowest selling price is ${itemFormat(lowestSellPrice)}` + newLine();
+        //     sellInfo += `* Average selling price is ${itemFormat(sellPrice)}` + newLine();
+        // } else {
+        //     sellInfo += '* Selling price data is unavailable' + newLine();
+        // }
+        // reply.addField('Sell', sellInfo);
+        //
+        // let buyInfo = '';
+        // if (buyPrice !== 'unknown') {
+        //     buyInfo += `* Highest buying price is ${itemFormat(highestBuyPrice)}` + newLine();
+        //     buyInfo += `* Average buying price is ${itemFormat(buyPrice)}` + newLine();
+        // } else {
+        //     buyInfo += '* Buying price data is unavailable' + newLine();
+        // }
+        // reply.addField('Buy', buyInfo);
+        //
+        // return {reply, itemData, locationName};
     }
 }
